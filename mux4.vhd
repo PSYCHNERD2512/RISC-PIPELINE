543bc72be564da7b3898ce2d0f941564175e59fb
Anay-25 <146884445+Anay-25@users.noreply.github.com>
2024-05-08 02:22:47 +0530

    Update mux4.vhd

--- mux4.vhd
+++ mux4.vhd
@@ -15,7 +15,11 @@ ARCHITECTURE arch OF mux_4 IS
     SIGNAL sel : STD_LOGIC_VECTOR(1 DOWNTO 0);
 BEGIN
     sel(0) <= (opcode(3) AND opcode(2) AND (NOT opcode(1))) OR ((NOT c) AND (NOT z)) OR (NOT opcode(3)) OR (opcode(3) AND opcode(2) AND opcode(1));
-    sel(1) <= (NOT opcode(3)) AND (opcode(3) AND (NOT opcode(2)) AND opcode(1) AND opcode(0)) OR (opcode(3) AND (NOT opcode(2)) AND (NOT opcode(1)) AND (NOT c) AND (NOT z)) OR (opcode(3) AND opcode(2) AND (NOT opcode(0)));
+    sel(1) <= (NOT opcode(3)) 
+				   OR (opcode(3) AND (NOT opcode(2)) AND opcode(1) AND opcode(0)) 
+	            OR (opcode(3) AND (NOT opcode(2)) AND (NOT opcode(1)) AND (NOT c) AND (NOT z)) 
+					OR (opcode(3) AND opcode(2) AND (NOT opcode(0))) 
+					OR (opcode(3) AND opcode(2) AND (NOT opcode(1)) AND opcode(0));
 
     o <= (((NOT sel(1)) AND (NOT sel(0))) AND a) OR (((NOT sel(1)) AND (sel(0))) AND b) OR (((NOT sel(1)) AND (sel(0))) AND c) OR (((sel(1)) AND (sel(0))) AND d);
 END arch;
